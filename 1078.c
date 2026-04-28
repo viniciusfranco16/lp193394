fea28a8109361d6143aa38cbdcf5d8b30128552e
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-04-28 19:21:18 -0300

    Update 1078.c

--- 1078.c
+++ 1078.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinicius Franco Silva>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
+Data        : 28/04/2026
+Objetivo    :
+Aprendizado : <<<Fazer um algoritimo que mostre a tabuada>>>
+-------------------------------------------------------------------------- */
+
+
 #include <stdio.h>
 
 int main(){
