32da0aef8dd9e46538067d8d6c47bc589c8588cb
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-04-28 19:20:08 -0300

    Update 1071.c

--- 1071.c
+++ 1071.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<Vinicius Franco Silva>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 28/04/2026
+Objetivo    : Ler dois valores inteiros e mostrar a soma.
+Aprendizado : <<<usar o if, for e else>>>
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
