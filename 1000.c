cee29cf8edf425fcd9c60e0c31b4992ba9c8cac0
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-04-28 19:15:00 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinicius Franco Silva>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 07/04/2026
+Objetivo    : 
+Aprendizado : <<<Aprender o basico da linguagem C>>>
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
