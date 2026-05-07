fb23c4c13e6bff975012d312c03fae226bf01259
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-05-07 14:11:26 -0300

    Create 1024.c

---
+++ 1024.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+ 
+int main() {
+ 
+   int N, n;
+   char aux, M [1001];
+  
+   scanf("%d", &n);
+
+   for(int i = 0; i < n; ++i);
+
+   for(int j = 0; j < n, ++j);
+    if()
+ 
+    return 0;
+}
