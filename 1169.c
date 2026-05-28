eabcf7feeafc0791c8e572d619ddda4b9e0a7a0b
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-05-28 13:51:27 -0300

    Update 1169.c

--- 1169.c
+++ 1169.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinicius Franco Silva>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1169
+Data        : 28/05/2026
+Objetivo    :
+Aprendizado : <<aprender a fazer uma progressão geometrica atraves da programação>>
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
