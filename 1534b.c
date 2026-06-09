2b6f3e40d225dca3502484733f0b0088ee641ae2
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-06-09 16:28:50 -0300

    Create 1534b.c

---
+++ 1534b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+#define MAXN 70
+ 
+int main(void) {
+    int N;
+    char mat[MAXN][MAXN];
+ 
+    while (scanf("%d", &N) == 1) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i + j == N - 1)
+                    *(*(mat + i) + j) = '2';
+                else if (i == j)
+                    *(*(mat + i) + j) = '1';
+                else
+                    *(*(mat + i) + j) = '3';
+            }
+        }
+ 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                putchar(*(*(mat + i) + j));
+            putchar('\n');
+        }
+    }
+ 
+    return 0;
+}
