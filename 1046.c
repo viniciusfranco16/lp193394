b584570a6feedc0e22684b8971de564245698449
viniciusfranco16 <vinicius.franco16@unifesp.br>
2026-06-23 17:28:05 -0300

    Update 1046.c

--- 1046.c
+++ 1046.c
@@ -14,14 +14,14 @@ Aprendizado : Uso de estruturas condicionais e manipulação de horas em problem
 
 int main()
 {
-    int hora_inicial, hora_final;
+    int hora_ini, hora_fim;
 
-    scanf("%d %d", &hora_inicial, &hora_final);
+    scanf("%d %d", &hora_ini, &hora_fim);
 
-    if (hora_final <= hora_inicial)
-        hora_final += 24;
+    if (hora_fim <= hora_ini)
+        hora_fim += 24;
 
-    printf("O JOGO DUROU %d HORA(S)\n", hora_final - hora_inicial);
+    printf("O JOGO DUROU %d HORA(S)\n", hora_fim - hora_ini);
 
     return 0;
 }
